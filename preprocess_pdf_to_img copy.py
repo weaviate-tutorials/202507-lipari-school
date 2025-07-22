3cd042e777082cbda9f3c17a70313c909be8b621
JP Hwang <me@jphwang.com>
2025-07-22 20:42:12 +0100

    updates

--- preprocess_pdf_to_img copy.py
+++ preprocess_pdf_to_img copy.py
@@ -22,12 +22,8 @@ def convert_pdf_to_images(src_file_path: Path, img_path: Path) -> list[Path]:
     return img_paths
 
 
-pdf_paths = [
-    Path("data/pdfs/hai_ai-index-report-2025_chapter2_final.pdf"),
-    Path("data/pdfs/howto-free-threading-python.pdf"),
-    Path("data/pdfs/manual_bosch_WGG254Z0GR.pdf")
-]
+pdf_path = Path("data/pdfs/manual_bosch_WGG254Z0GR.pdf")
+# pdf_path = Path("data/pdfs/howto-free-threading-python.pdf")
 img_dir = Path("data/imgs")
 
-for pdf_path in pdf_paths:
-    convert_pdf_to_images(pdf_path, img_dir)
+convert_pdf_to_images(pdf_path, img_dir)
